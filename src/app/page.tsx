73c2f2cba735004bbf86bccc32517e3418d251c1
jackschu <js9pa@virginia.edu>
2023-05-25 00:45:13 -0400

    lower case it all, also make text size and padding dynamic for main content, also introduce blur

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,30 @@
 import Image from 'next/image'
+import profilePic from '../../public/pic.jpg'
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-screen flex-col items-center justify-between p-16 pt-24 lg:p-24">
             <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
                 <p className="fixed left-0 top-0 flex w-full justify-center  bg-background bg-gradient-to-b from-secondary-button  pb-6 pt-8 backdrop-blur-2xl lg:static lg:w-auto lg:rounded-xl lg:p-4 lg:shadow-md lg:shadow-primary-button">
-                    An exercise in&nbsp;
+                    an exercise in&nbsp;
                     <code className="font-mono font-bold">Next.js and tailwind</code>
                 </p>
                 <div className="fixed bottom-0 left-0 flex h-auto w-full justify-center border-t-2 border-secondary-button py-2 backdrop-blur-sm lg:static  lg:w-auto lg:items-end lg:border-none lg:bg-none">
-                    By&nbsp;<span className="italic">Jack Schumann</span>
+                    by&nbsp;<span className="italic">jack schumann</span>
                 </div>
             </div>
 
-            <div className="flex flex-col items-center gap-8 rounded-md hover:border-2 hover:border-accent lg:flex-row lg:gap-16">
-                <p className="text-5xl">👋 Hi, I'm Jack</p>
+            <div className="flex flex-col items-center gap-6 rounded-md hover:border-2 hover:border-accent lg:flex-row lg:gap-16">
+                <p className="text-center text-4xl lg:text-5xl">
+                    {"👋 hi i'm "}
+                    {'jack'}
+                </p>
                 <Image
-                    className="rounded-full shadow-lg shadow-[#8a8eb2]/40 dark:shadow-[#8a8eb2]/30" //"
-                    src={'/pic.jpg'}
+                    className="w-80 rounded-full shadow-lg shadow-[#8a8eb2]/40 dark:shadow-[#8a8eb2]/30" //"
+                    src={profilePic}
                     alt={'profile pic of jack'}
-                    width={300}
-                    height={300}
+                    placeholder="blur"
+                    blurDataURL="/pic_blur.jpg"
                 />
             </div>
 
@@ -32,13 +36,13 @@ export default function Home() {
                     rel="noopener noreferrer"
                 >
                     <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Github{' '}
+                        github{' '}
                         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                             -&gt;
                         </span>
                     </h2>
                     <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        Explore other projects made by me.
+                        explore other projects made by me.
                     </p>
                 </a>
 
@@ -49,13 +53,13 @@ export default function Home() {
                     rel="noopener noreferrer"
                 >
                     <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Blog{' '}
+                        blog{' '}
                         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                             -&gt;
                         </span>
                     </h2>
                     <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        Lorem ipsum is the best, but I hope my content will be better.
+                        lorem ipsum is the best, but i hope my content will be better.
                     </p>
                 </a>
 
@@ -66,13 +70,13 @@ export default function Home() {
                     rel="noopener noreferrer"
                 >
                     <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Directions{' '}
+                        directions{' '}
                         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                             -&gt;
                         </span>
                     </h2>
                     <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        Parking instructions are included (maybe)
+                        parking instructions are included (maybe)
                     </p>
                 </a>
 
@@ -83,13 +87,13 @@ export default function Home() {
                     rel="noopener noreferrer"
                 >
                     <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Version 1{' '}
+                        version 1{' '}
                         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                             -&gt;
                         </span>
                     </h2>
                     <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        My site had a proto-site, you can go check it out.
+                        my site had a proto-site, you can go check it out.
                     </p>
                 </a>
             </div>
