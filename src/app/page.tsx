7a2f290fc913dbb3ed490f50b87d0d5e831b8b60
jackschu <js9pa@virginia.edu>
2023-05-28 01:43:43 -0400

    auth seems to work, but i made a mess

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,71 @@
+'use client'
+
 import Image from 'next/image'
 import profilePic from '../../public/pic.jpg'
 import Header from './header'
 import HomeLinks from './homeLinks'
+import { useEffect, useState } from 'react'
+import { Amplify, Hub } from '@aws-amplify/core'
+import { CognitoHostedUIIdentityProvider, Auth } from '@aws-amplify/auth'
+import awsConfig from './../aws-exports'
+
+const isLocalhost = true
+
+// Assuming you have two redirect URIs, and the first is for localhost and second is for production
+const [productionRedirectSignIn, localRedirectSignIn] = awsConfig.oauth.redirectSignIn.split(',')
+
+const [productionRedirectSignOut, localRedirectSignOut] = awsConfig.oauth.redirectSignOut.split(',')
+
+const updatedAwsConfig = {
+    ...awsConfig,
+    oauth: {
+        ...awsConfig.oauth,
+        redirectSignIn: isLocalhost ? localRedirectSignIn : productionRedirectSignIn,
+        redirectSignOut: isLocalhost ? localRedirectSignOut : productionRedirectSignOut,
+    },
+}
+
+Amplify.configure(updatedAwsConfig)
 
 export default function Home() {
+    const [user, setUser] = useState(null)
+    const [customState, setCustomState] = useState(null)
+
+    useEffect(() => {
+        const unsubscribe = Hub.listen('auth', ({ payload: { event, data } }) => {
+            switch (event) {
+                case 'signIn':
+                    setUser(data)
+                    break
+                case 'signOut':
+                    setUser(null)
+                    break
+                case 'customOAuthState':
+                    setCustomState(data)
+            }
+        })
+
+        Auth.currentAuthenticatedUser()
+            .then((currentUser) => setUser(currentUser))
+            .catch(() => console.log('Not signed in'))
+
+        return unsubscribe
+    }, [])
+
+    console.log(user)
     return (
         <main className="flex min-h-screen flex-col items-center justify-between lg:p-24 lg:pt-20">
             <Header />
+            {user ? user.attributes.name : 'nope'}
+
+            <button
+                onClick={() =>
+                    Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Google })
+                }
+            >
+                Open Google
+            </button>
+            <button onClick={() => Auth.signOut()}>Sign Out</button>
             <div className="group flex flex-col items-center gap-10 rounded-md pb-14 pt-10 lg:flex-row lg:gap-16 lg:pt-0">
                 <div className="flex flex-row gap-2">
                     <div className="flex flex-col lg:gap-1">
