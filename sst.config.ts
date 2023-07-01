ccb276e1a2bf5fdcb9b3dba8029ef72801533730
jackschu <js9pa@virginia.edu>
2023-07-01 16:28:40 -0400

    sst seems to work for an upload file example

---
+++ sst.config.ts
@@ -0,0 +1,21 @@
+import { SSTConfig } from 'sst'
+import { Bucket, NextjsSite } from 'sst/constructs'
+
+export default {
+    config(_input) {
+        return {
+            name: 'next-app',
+            region: 'us-east-1',
+        }
+    },
+    stacks(app) {
+        app.stack(function Site({ stack }) {
+            const bucket = new Bucket(stack, 'public')
+            const site = new NextjsSite(stack, 'site', { bind: [bucket] })
+
+            stack.addOutputs({
+                SiteUrl: site.url,
+            })
+        })
+    },
+} satisfies SSTConfig
